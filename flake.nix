7dbee02e23df584cf7282c75e233a5dca1fac968
ok-nick <nick.libraries@gmail.com>
2023-09-17 14:52:47 -0400

    complete nix environment

--- flake.nix
+++ flake.nix
@@ -1,22 +1,53 @@
 {
-  description = "Python 3.9 development environment";
+  description = "UB-NSSD development environment";
 
   outputs = {
     self,
     nixpkgs,
   }: let
     system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
+        cudaSupport = true;
+      };
+    };
   in {
     devShells.${system}.default = pkgs.mkShellNoCC {
       LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
 
-      buildInputs = with pkgs; [
+      buildInputs = with pkgs; let
+        whisper-at = python310Packages.buildPythonPackage rec {
+          pname = "whisper-at";
+          version = "0.5";
+
+          src = python310Packages.fetchPypi {
+            inherit pname version;
+            sha256 = "f685f7bd71ab262b7307d9bd1e0eea5eac75b9ba34add422ea9e97937f21b19f";
+          };
+
+          propagatedBuildInputs = with python310Packages; [
+            numba
+            numpy
+            torch-bin
+            tqdm
+            more-itertools
+            tiktoken
+            openai-triton-bin
+          ];
+
+          doCheck = false;
+        };
+      in [
         ffmpeg
 
-        python311
-        python311Packages.pip
-        python311Packages.virtualenv
+        whisper-at
+        python310
+        python310Packages.matplotlib
+        python310Packages.pytube
+        python310Packages.pandas
+        python310Packages.torchaudio-bin
       ];
     };
   };
