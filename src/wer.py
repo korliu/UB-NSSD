79715548f283ba1d79a414486b7f25648de16ea7
korliu <koreyliu1221@gmail.com>
2023-09-19 00:36:37 -0400

    function for wer

--- src/wer.py
+++ src/wer.py
@@ -4,6 +4,15 @@ from torcheval.metrics import WordErrorRate
 metric = WordErrorRate()
 
 # TODO
-metric.update(["text1", "text2"])
+# metric.update("text1", "text2")
 
-print(metric.compute())
+# print(metric.compute())
+
+
+def compute_wer(expected_text: str, predicted_text: str):
+
+    metric = WordErrorRate()
+
+    metric.update(expected_text, predicted_text)
+
+    return metric.compute()
