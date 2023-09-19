2c3b347f72fdce0ee46df56a18fe2d82d0898a7a
ok-nick <nick.libraries@gmail.com>
2023-09-19 01:51:57 -0400

    add deps

--- flake.nix
+++ flake.nix
@@ -17,6 +17,7 @@
     devShells.${system}.default = pkgs.mkShellNoCC {
       LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
 
+      # TODO: https://cyberchris.xyz/posts/nix-python-pyright/
       buildInputs = with pkgs; let
         whisper-at = python310Packages.buildPythonPackage rec {
           pname = "whisper-at";
@@ -39,15 +40,47 @@
 
           doCheck = false;
         };
+        torcheval = python310Packages.buildPythonPackage rec {
+          pname = "torcheval";
+          version = "0.0.6";
+
+          src = fetchFromGitHub {
+            owner = "pytorch";
+            repo = "${pname}";
+            rev = "${version}";
+            sha256 = "FnMSPU8tjXegLH4speeyD8UDrKSvjf8STftt7aXTuJI=";
+          };
+
+          propagatedBuildInputs = with python310Packages; [
+            typing-extensions
+          ];
+
+          doCheck = false;
+        };
+        pytube = python310Packages.buildPythonPackage rec {
+          pname = "pytube";
+          version = "15.0.0";
+
+          src = fetchFromGitHub {
+            owner = "${pname}";
+            repo = "${pname}";
+            rev = "v${version}";
+            sha256 = "Nvs/YlOjk/P5nd1kpUnCM2n6yiEaqZP830UQI0Ug1rk=";
+          };
+
+          doCheck = false;
+        };
       in [
         ffmpeg
 
-        whisper-at
         python310
         python310Packages.matplotlib
-        python310Packages.pytube
+        # python310Packages.pytube
         python310Packages.pandas
         python310Packages.torchaudio-bin
+        whisper-at
+        torcheval
+        pytube
       ];
     };
   };
