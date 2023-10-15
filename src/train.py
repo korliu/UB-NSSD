c2995083a56dd6d736006e9601f399e63a802817
ok-nick <nick.libraries@gmail.com>
2023-10-15 16:04:27 -0400

    progress training yamnet

--- src/train.py
+++ src/train.py
@@ -11,26 +11,37 @@ import tensorflow_io as tfio
 
 import download_manual  # noqa: F401
 
+# 80% train, 20% validate/test (for now)
+TRAIN_RATIO = 0.8
+
 yamnet_model = hub.load("https://tfhub.dev/google/yamnet/1")
 
 pd_data = pd.read_csv(download_manual.CSV_PATH)
 map_class_to_id = {k: i for i, k in enumerate(pd_data["positive_labels"].unique())}
+my_classes = map_class_to_id.keys()
 
 filtered_pd = pd_data
 filtered_pd["target"] = filtered_pd["positive_labels"].apply(
     lambda name: map_class_to_id[name]
 )
 filtered_pd["filename"] = filtered_pd.apply(
-    lambda row: download_manual.OUTPUT_DIR
-    / download_manual.format_path(
-        row["YTID"], row["start_seconds"], row["end_seconds"]
+    lambda row: str(
+        download_manual.OUTPUT_DIR
+        / download_manual.format_path(
+            row["YTID"], row["start_seconds"], row["end_seconds"]
+        )
     ),
     axis=1,
 )
 
+train = filtered_pd.sample(frac=TRAIN_RATIO)
+test = filtered_pd.drop(train.index)
+train["fold"] = 1
+test["fold"] = 2
+
+filtered_pd = pd.concat([train, test])
 print(filtered_pd.head())
 
-# TODO: fold
 filenames = filtered_pd["filename"]
 targets = filtered_pd["target"]
 folds = filtered_pd["fold"]
@@ -70,3 +81,52 @@ def extract_embedding(wav_data, label, fold):
 
 # extract embedding
 main_ds = main_ds.map(extract_embedding).unbatch()
+
+cached_ds = main_ds.cache()
+train_ds = cached_ds.filter(lambda embedding, label, fold: fold < 2)
+val_ds = cached_ds.filter(lambda embedding, label, fold: fold == 2)
+test_ds = cached_ds.filter(lambda embedding, label, fold: fold == 2)
+
+# remove the folds column now that it's not needed anymore
+remove_fold_column = lambda embedding, label, fold: (embedding, label)
+
+train_ds = train_ds.map(remove_fold_column)
+val_ds = val_ds.map(remove_fold_column)
+test_ds = test_ds.map(remove_fold_column)
+
+train_ds = train_ds.cache().shuffle(1000).batch(32).prefetch(tf.data.AUTOTUNE)
+val_ds = val_ds.cache().batch(32).prefetch(tf.data.AUTOTUNE)
+test_ds = test_ds.cache().batch(32).prefetch(tf.data.AUTOTUNE)
+my_model = tf.keras.Sequential(
+    [
+        tf.keras.layers.Input(shape=(1024), dtype=tf.float32, name="input_embedding"),
+        tf.keras.layers.Dense(512, activation="relu"),
+        tf.keras.layers.Dense(len(my_classes)),
+    ],
+    name="my_model",
+)
+
+my_model.summary()
+
+my_model.compile(
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer="adam",
+    metrics=["accuracy"],
+)
+
+callback = tf.keras.callbacks.EarlyStopping(
+    monitor="loss", patience=3, restore_best_weights=True
+)
+
+history = my_model.fit(train_ds, epochs=20, validation_data=val_ds, callbacks=callback)
+
+loss, accuracy = my_model.evaluate(test_ds)
+
+print("Loss: ", loss)
+print("Accuracy: ", accuracy)
+
+# scores, embeddings, spectrogram = yamnet_model(testing_wav_data)
+# result = my_model(embeddings).numpy()
+
+# inferred_class = my_classes[result.mean(axis=0).argmax()]
+# print(f'The main sound is: {inferred_class}')
