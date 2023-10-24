61ef5647dbe540ac3987b02632fc3e68be7c88a1
korliu <koreyliu1221@gmail.com>
2023-10-24 04:06:26 -0400

    downloads manual_data and combines with eating sound collection csvs

--- src/download_yt.py
+++ src/download_yt.py
@@ -2,11 +2,15 @@ import utils
 import csv
 from pathlib import Path
 import soundfile
+import pandas as pd
+# import numpy as np
+from numpy import nan
 
 CSV_PATH = Path("datasets/manual_data.csv")
 OUTPUT_DIR = Path("datasets/manual")
 OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
+Path.home()
 
 def format_path(youtube_id, start, end):
     # TODO: floor/ceil instead of int
@@ -43,3 +47,33 @@ with open(CSV_PATH) as f:
             soundfile.write(path, data, samplerate, subtype="PCM_16")
 
         print(output_path)
+
+
+YT_DATA = CSV_PATH
+FOOD_DATA = Path("datasets/foods_data.csv")
+
+ALL_DATA = Path("datasets/all_data.csv")
+
+
+food_df = pd.read_csv(FOOD_DATA)
+
+# clean food_df
+food_df = food_df[food_df['variant'] != "na"]
+
+# print(food_df)
+
+# format yt_df
+yt_df = pd.read_csv(YT_DATA)
+yt_df['path'] = yt_df.apply(lambda x: (OUTPUT_DIR / format_path(x['YTID'], x['start_seconds'], x['end_seconds'])).as_posix(), axis=1)
+# all are females
+yt_df['sex'] = "female"
+yt_df["other_labels"] = nan
+yt_df.rename(columns={"positive_labels": "variant"}, inplace=True)
+
+expected_cols = ["path","variant","food","sex","other_labels"]
+
+yt_df = pd.DataFrame(yt_df, columns = expected_cols)
+
+all_df = pd.concat([food_df,yt_df],ignore_index=True)
+
+all_df.to_csv(ALL_DATA,index=False)
