2181598fa89637d9f8a15fde17e0adec602ae80d
Korey Liu <koreyliu1221@gmail.com>
2023-12-26 18:31:36 -0500

    model takes in wavs, testing, fixed arg parsing

--- src/transcribe.py
+++ src/transcribe.py
@@ -26,20 +26,31 @@ parser.add_argument("audio_path",
 # parser.add_argument("--sr", action="store_const") 
 
 # window size
-parser.add_argument("--size", action=STORE_CONST, const=0.98,
+parser.add_argument("--size", nargs='?', const=1, type=float, default=0.98,
                     help="Size of prediction window, in seconds, default=0.98")
 # step size
-parser.add_argument("--step", action=STORE_CONST, const=0.10, 
+parser.add_argument("--step", nargs='?', const=1, type=float, default=0.10,
                     help="Step to move window, in seconds, default=0.10")
 
 parser.add_argument("--save_path", action=STORE,
                     help="file path to store the graph")
 
-parser.add_argument("--version", action=STORE_CONST, const="only_intake",
+parser.add_argument("--model_version", nargs='?', const=1, default="only_intake",
                     help="Version of the model to transcribe with. \
                         [only_intake, all], default=only_intake")
 
 
 args = parser.parse_args()
+print(args)
 
-model_path = Path("models",args.version)
+model_path = Path("models",args.model_version)
+
+model = tf.keras.saving.load_model(model_path)
+
+model.summary()
+
+# model takes in wavs i believe?
+audio = training.load_wav_16k_mono(args.audio_path)
+test = model.predict(audio)
+
+print(test)
